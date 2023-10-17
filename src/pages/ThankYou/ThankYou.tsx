68856b6158684ca763dc48a48cd9188bc14cbe55
keerthyeb <keerthythakudu1997@gmail.com>
2023-10-17 21:31:15 +0530

    Modify thank you page

--- src/pages/ThankYou/ThankYou.tsx
+++ src/pages/ThankYou/ThankYou.tsx
@@ -5,13 +5,36 @@ function ThankYou() {
   return (
     <Body>
       <Container>
-        <Typography variant="h4">
-          Thank you for inquiring about your trip of a lifetime with us. Our
-          team of experts will be in touch soon to help craft the perfect
-          vacation for you.{" "}
-        </Typography>
-        <Typography variant="h5"> Sincerely, </Typography>
-        <Typography variant="h5"> Enchanting Travels</Typography>
+        <div style={{ marginBlockEnd: "17%" }}></div>
+        <div
+          style={{
+            minHeight: "35vh",
+            padding: "40px",
+            margin: "10px",
+            display: "flex",
+            background: "white",
+            borderRadius: "10px",
+            justifyContent: "space-evenly",
+            flexDirection: "column",
+            boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+          }}
+        >
+          <Typography style={{ fontFamily: "cursive" }} variant="h4">
+            Thank you for inquiring about your trip of a lifetime with us. Our
+            team of experts will be in touch soon to help craft the perfect
+            vacation for you.
+          </Typography>
+          <div>
+            <Typography style={{ fontFamily: "cursive" }} variant="h5">
+              {" "}
+              Sincerely,{" "}
+            </Typography>
+            <Typography style={{ fontFamily: "cursive" }} variant="h5">
+              {" "}
+              Enchanting Travels
+            </Typography>
+          </div>
+        </div>
       </Container>
     </Body>
   );
