2481e468fb26995d03255dbb5790e1c60df5b404
keerthyeb <keerthythakudu1997@gmail.com>
2023-10-17 10:16:24 +0530

    Add Material UI theme

--- config/jest.config.cjs
+++ config/jest.config.cjs
@@ -1,37 +1,37 @@
 module.exports = {
-    "rootDir": "../",
-    "testEnvironment": "jsdom",
-    "moduleNameMapper": {
-        "^.+\\.svg$": "jest-svg-transformer",
-        "^.+\\.(css|less|scss)$": "identity-obj-proxy",
-        '\^.+\.(png|jpg|webp|ttf|woff|woff2|svg|mp4)$': 'jest-transform-stub'
-    },
-    "setupFilesAfterEnv": [
-        "<rootDir>/config/setupTests.js"
+  "rootDir": "../",
+  "testEnvironment": "jsdom",
+  "moduleNameMapper": {
+    "^.+\\.svg$": "jest-svg-transformer",
+    "^.+\\.(css|less|scss)$": "identity-obj-proxy",
+    '\^.+\.(png|jpg|webp|ttf|woff|woff2|svg|mp4)$': 'jest-transform-stub'
+  },
+  "setupFilesAfterEnv": [
+    "<rootDir>/config/setupTests.js"
+  ],
+  reporters: [
+    "default",
+    [
+      "jest-html-reporters",
+      {
+        publicPath: "./coverage/html-report",
+        filename: "report.html",
+      },
     ],
-    reporters: [
-        "default",
-        [
-            "jest-html-reporters",
-            {
-                publicPath: "./coverage/html-report",
-                filename: "report.html",
-            },
-        ],
-    ],
-    collectCoverageFrom: [
-        '!*.d.ts',
-        "src/**/*.{js,ts,jsx,tsx}",
-        "!src/main.tsx",
-    ],
-    coverageReporters: ["text"],
+  ],
+  collectCoverageFrom: [
+    '!*.d.ts',
+    "src/**/*.{js,ts,jsx,tsx}",
+    "!src/main.tsx",
+  ],
+  coverageReporters: ["text"],
 
-    coverageThreshold: {
-        global: {
-            branches: 90,
-            functions: 90,
-            lines: 90,
-            statements: 90,
-        },
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
+  },
 };
---
+++ src/style/theme.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#175274",
+    },
+  },
+});
+
+export default theme;
