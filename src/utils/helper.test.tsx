3cfa096447da4e92c31b841934238e7064df1eed
keerthyeb <keerthythakudu1997@gmail.com>
2023-10-18 08:47:10 +0530

    Add more test and fix failing test

---
+++ src/utils/helper.test.tsx
@@ -0,0 +1,31 @@
+import { isValid } from "./helper";
+
+describe('Validation Function', () => {
+  it('should return true for valid inputs', () => {
+    const validName = 'John Doe';
+    const validEmail = 'johndoe@example.com';
+    const validPhoneNumber = '1234567890';
+    expect(isValid(validName, validEmail, validPhoneNumber)).toBe(false);
+  });
+
+  it('should return false for invalid email format', () => {
+    const invalidName = 'Jane Doe';
+    const invalidEmail = 'janedoeexample.com';
+    const validPhoneNumber = '9876543210';
+    expect(isValid(invalidName, invalidEmail, validPhoneNumber)).toBe(true);
+  });
+
+  it('should return false for invalid phone number format', () => {
+    const validName = 'Michael Scott';
+    const validEmail = 'michaelscott@example.com';
+    const invalidPhoneNumber = '12345';
+    expect(isValid(validName, validEmail, invalidPhoneNumber)).toBe(false);
+  });
+
+  it('should return true if name is empty', () => {
+    const emptyName = '';
+    const validEmail = 'emptyname@example.com';
+    const validPhoneNumber = '9876543210';
+    expect(isValid(emptyName, validEmail, validPhoneNumber)).toBe(true);
+  });
+});
